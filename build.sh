0df0200e0d65086fccd48b893f67fe7c7b9429e3
Major Hayden <major@mhtx.net>
2021-06-03 16:40:30 -0500

    Disable centos 8 stream in build test

--- build.sh
+++ build.sh
@@ -12,10 +12,9 @@ sudo mock -r /etc/mock/fedora-rawhide-x86_64.cfg --with=check \
     $SRPM_NAME
 rpmlint /var/lib/mock/fedora-rawhide-x86_64/result/*.rpm
 
-sudo mock -r /etc/mock/centos-stream-8-x86_64.cfg --with=check \
-    -a https://download.copr.fedorainfracloud.org/results/mhayden/azure-cli/fedora-rawhide-x86_64/ \
-    --postinstall \
-    $SRPM_NAME
-rpmlint /var/lib/mock/centos-stream-8-x86_64/result/*.rpm
+# sudo mock -r /etc/mock/centos-stream-8-x86_64.cfg --with=check \
+#     --postinstall \
+#     $SRPM_NAME
+# rpmlint /var/lib/mock/centos-stream-8-x86_64/result/*.rpm
 
 # copr build --nowait azure-cli $SRPM_NAME
